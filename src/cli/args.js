241802c8ddc7aafed86878ef1150641e926c9307
Max <veyronmclord@gmail.com>
2025-05-17 16:59:49 +0600

    feat: implement functions for command line interface tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
+/*
+  args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't
+  need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
+*/
 const parseArgs = () => {
-  // Write your code here
-};
+  const args = process.argv.slice(2) // Get command line arguments excluding the first two elements (node and script path)
+  const parsedArgs = {}
+  
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace('--', '')
+    const value = args[i + 1]
+    parsedArgs[key] = value
+  }
 
-parseArgs();
+  Object.entries(parsedArgs).forEach(([key, value]) => {
+    console.log(`${key} is ${value}`)
+  })
+}
+
+parseArgs()
