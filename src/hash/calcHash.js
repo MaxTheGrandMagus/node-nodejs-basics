95cddf9b4257612f0f2a8340c5c8ca886e186032
Max <veyronmclord@gmail.com>
2025-05-17 18:21:15 +0600

    feat: implement function for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+/*
+  You should implement several functions in dedicated files
+  calcHash.js - implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using
+  Streams API
+*/
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const calculateHash = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileToCalculateHashFor = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  const hash = createHash('sha256');
+  const inputStream = createReadStream(fileToCalculateHashFor);
+
+  await pipeline(inputStream, hash);
+
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
