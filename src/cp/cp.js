59af11be2ab6c3327e44a7f00fc9984559150d4e
Max <veyronmclord@gmail.com>
2025-05-18 19:51:53 +0600

    feat: implement child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+/*
+  cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file script.js,
+  passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
+    child process stdin should receive input from master process stdin
+    child process stdout should send data to master process stdout
+*/
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { fork } from 'child_process'
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
-};
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const scriptFile = path.join(__dirname, 'files/script.js')
+
+  fork(scriptFile, args)
+}
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['pen', 'pineapple', 'apple', 'pen'])
