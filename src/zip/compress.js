a4d504fbc46dde4e13bab33d4a8c9f740f916aae
Max <veyronmclord@gmail.com>
2025-05-18 19:01:52 +0600

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+// compress.js - implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+import os from 'os';
+
 const compress = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToCompress = path.join(__dirname, 'files/fileToCompress.txt');
+  const destinationFile = path.join(__dirname, 'files/archive.gz');
+
+  const sourceStream = fs.createReadStream(fileToCompress);
+  const destinationStream = fs.createWriteStream(destinationFile);
+  const gzipStream = zlib.createGzip();
+
+  sourceStream.pipe(gzipStream).pipe(destinationStream);
+
+  destinationStream.on('finish', () => {
+    process.stdout.write(`File compressed to ${destinationFile}${os.EOL}`);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+/*
+  decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before
+  compression using zlib and Streams API
+*/
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+import os from 'os';
+
 const decompress = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const archiveFile = path.join(__dirname, 'files/archive.gz');
+  const destinationFile = path.join(__dirname, 'files/fileToCompress.txt');
+
+  const sourceStream = fs.createReadStream(archiveFile);
+  const destinationStream = fs.createWriteStream(destinationFile);
+  const gunzipStream = zlib.createGunzip();
+
+  sourceStream.pipe(gunzipStream).pipe(destinationStream);
+
+  destinationStream.on('finish', () => {
+    process.stdout.write(`File decompressed to ${destinationFile}${os.EOL}`);
+  });
 };
 
 await decompress();
